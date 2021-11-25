709a535024402b60f2c9d14a95593fd1622a77db
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2021-11-26 00:14:09 +0700

    update backend loguru MantApp
    update backend loguru MantApp

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
 from logging import debug
 from flask import Flask
-
+from loguru import logger
+# from db import db_mongo
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World'
+    logger.debug("That's it, beautiful and simple logging!")
+    logger.add("log/log_data.log", enqueue=True)
+
 
 
 if __name__ == '__main__':
