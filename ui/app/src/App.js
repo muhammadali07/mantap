016979b3d2bd64020eb348f21a188d1cca88b72b
muhammadali07 <muhalibakhtiar@gmail.com>
2022-03-06 15:35:07 +0700

    add tailwind/form

--- ui/app/src/App.js
+++ ui/app/src/App.js
@@ -89,6 +89,15 @@ function App() {
           <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
           <form action="#" method="POST">
             <div class="m-8 relative space-y-4">
+            <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+                <div class="flex-1">
+                  {/* <div class="h-4 w-48 bg-gray-300 rounded"></div> */}
+                  <input id="email" name="email" type="text" autocomplete="text" required class="" placeholder="create a username" />
+                </div>
+                {/* <div>
+                <div class="w-24 h-6 rounded-lg bg-purple-300"></div>
+              </div> */}
+              </div>
               <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
                 <div class="flex-1">
                   {/* <div class="h-4 w-48 bg-gray-300 rounded"></div> */}
