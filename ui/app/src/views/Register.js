850e9ac3b236ee123971b2cb42f6d015bd5659e8
muhammadali07 <muhalibakhtiar@gmail.com>
2022-03-08 18:21:49 +0700

    add register page

--- ui/app/src/views/Register.js
+++ ui/app/src/views/Register.js
@@ -80,10 +80,10 @@ function App() {
         <div class="relative w-full max-w-lg">
           <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign In</h2>
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your account</h2>
             <p class="mt-2 text-center text-sm text-gray-600 max-w">
-              Don't have a account ?
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"> Sign Up</a>
+              Already registered?
+              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">Sign in</a>
             </p>
           </div>
           <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
@@ -91,10 +91,19 @@ function App() {
           <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
           <form action="#" method="POST">
             <div class="m-8 relative space-y-4">
+            <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+                <div class="flex-1">
+                  {/* <div class="h-4 w-48 bg-gray-300 rounded"></div> */}
+                  <input id="email" name="email" type="text" autocomplete="text" required class="" placeholder="create a username" />
+                </div>
+                {/* <div>
+                <div class="w-24 h-6 rounded-lg bg-purple-300"></div>
+              </div> */}
+              </div>
               <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
                 <div class="flex-1">
                   {/* <div class="h-4 w-48 bg-gray-300 rounded"></div> */}
-                  <input id="email" name="email" type="email" autocomplete="email" required class="" placeholder="username/email" />
+                  <input id="email" name="email" type="email" autocomplete="email" required class="" placeholder="type your email" />
                 </div>
                 {/* <div>
                 <div class="w-24 h-6 rounded-lg bg-purple-300"></div>
@@ -103,17 +112,35 @@ function App() {
               <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
                 <div class="flex-1">
                   {/* <div class="h-4 w-56 bg-gray-300 rounded"></div> */}
-                  <input id="password" name="password" type="password" autocomplete="current-password" required class="" placeholder="password" />
+                  <input id="password" name="password" type="password" autocomplete="current-password" required class="" placeholder="create a new password" />
                 </div>
                 {/* <div>
                 <div class="w-20 h-6 rounded-lg bg-yellow-300"></div>
               </div> */}
               </div>
+              <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+                <div class="flex-1">
+                  {/* <div class="h-4 w-56 bg-gray-300 rounded"></div> */}
+                  <input id="password" name="password" type="password" autocomplete="current-password" required class="" placeholder="confirm a new password" />
+                </div>
+                {/* <div>
+                <div class="w-20 h-6 rounded-lg bg-yellow-300"></div>
+              </div> */}
+              </div>
+              <div class="flex items-center">
+                <input id="terms-and-privacy" name="terms-and-privacy" type="checkbox" class="" />
+                <label for="terms-and-privacy" class="ml-2 block text-sm text-gray-900"
+                >I agree to the
+                  <a href="#" class="text-indigo-600 hover:text-indigo-500">Terms</a>
+                  and
+                  <a href="#" class="text-indigo-600 hover:text-indigo-500">Privacy Policy</a>.
+                </label>
+              </div>
               <div class='flex justify-center'>
-                <button type="submit" class="w-full flex justify-center py-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign In</button>
+                <button type="submit" class="w-full flex justify-center py-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign up</button>
               </div>
               <div className='mt-2 sm:mx-auto sm:w-full sm:max-w-md'>
-                <p class="mt-2 text-center text-sm text-gray-600 max-w">Or Sign In with :</p>
+                <p class="mt-2 text-center text-sm text-gray-600 max-w">Or Sign Up with :</p>
               </div>
               <div class="p-4 bg-gray-200 rounded-full flex items-center justify-between space-x-8">
                 <div class="flex-1 grid grid-cols-4 gap-4">
